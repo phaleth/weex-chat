08a533e3831d84b23ac385871a8d09f0678a80a5
phaleth <29838606+phaleth@users.noreply.github.com>
2023-08-20 16:47:58 +0200

    Remove heroicons completely

--- lib/weex_chat_web/components/core_components.ex
+++ lib/weex_chat_web/components/core_components.ex
@@ -11,8 +11,6 @@ defmodule WeexChatWeb.CoreComponents do
   The default components use Tailwind CSS, a utility-first CSS framework.
   See the [Tailwind CSS documentation](https://tailwindcss.com) to learn
   how to customize them or feel free to swap in another framework altogether.
-
-  Icons are provided by [heroicons](https://heroicons.com). See `icon/1` for usage.
   """
   use Phoenix.Component
 
@@ -75,7 +73,7 @@ defmodule WeexChatWeb.CoreComponents do
                   class="-m-3 flex-none p-3 opacity-20 hover:opacity-40"
                   aria-label={gettext("close")}
                 >
-                  <.icon name="hero-x-mark-solid" class="h-5 w-5" />
+                  <div class="h-5 w-5">&#9776;</div>
                 </button>
               </div>
               <div id={"#{@id}-content"}>
@@ -120,13 +118,13 @@ defmodule WeexChatWeb.CoreComponents do
       {@rest}
     >
       <p :if={@title} class="flex items-center gap-1.5 text-sm font-semibold leading-6">
-        <.icon :if={@kind == :info} name="hero-information-circle-mini" class="h-4 w-4" />
-        <.icon :if={@kind == :error} name="hero-exclamation-circle-mini" class="h-4 w-4" />
+        <div :if={@kind == :info} class="h-4 w-4">&#128712;</div>
+        <div :if={@kind == :error} class="h-4 w-4">!&#x20DD;</div>
         <%= @title %>
       </p>
       <p class="mt-2 text-sm leading-5"><%= msg %></p>
       <button type="button" class="group absolute top-1 right-1 p-2" aria-label={gettext("close")}>
-        <.icon name="hero-x-mark-solid" class="h-5 w-5 opacity-40 group-hover:opacity-70" />
+        <div class="h-5 w-5 opacity-40 group-hover:opacity-70">&#9776;</div>
       </button>
     </div>
     """
@@ -153,7 +151,8 @@ defmodule WeexChatWeb.CoreComponents do
       phx-connected={hide("#client-error")}
       hidden
     >
-      Attempting to reconnect <.icon name="hero-arrow-path" class="ml-1 h-3 w-3 animate-spin" />
+      Attempting to reconnect
+      <div class="ml-1 h-3 w-3 animate-spin">&#128472;</div>
     </.flash>
 
     <.flash
@@ -165,7 +164,7 @@ defmodule WeexChatWeb.CoreComponents do
       hidden
     >
       Hang in there while we get back on track
-      <.icon name="hero-arrow-path" class="ml-1 h-3 w-3 animate-spin" />
+      <div class="ml-1 h-3 w-3 animate-spin">&#128472;</div>
     </.flash>
     """
   end
@@ -403,7 +402,7 @@ defmodule WeexChatWeb.CoreComponents do
   def error(assigns) do
     ~H"""
     <p class="mt-3 flex gap-3 text-sm leading-6 text-rose-600 phx-no-feedback:hidden">
-      <.icon name="hero-exclamation-circle-mini" class="mt-0.5 h-5 w-5 flex-none" />
+      <div class="mt-0.5 h-5 w-5 flex-none">!&#x20DD;</div>
       <%= render_slot(@inner_block) %>
     </p>
     """
@@ -554,37 +553,10 @@ defmodule WeexChatWeb.CoreComponents do
         navigate={@navigate}
         class="text-sm font-semibold leading-6 text-zinc-900 hover:text-zinc-700"
       >
-        <.icon name="hero-arrow-left-solid" class="h-3 w-3" />
+        <div class="h-3 w-3">&#129188;</div>
         <%= render_slot(@inner_block) %>
       </.link>
     </div>
-    """
-  end
-
-  @doc """
-  Renders a [Heroicon](https://heroicons.com).
-
-  Heroicons come in three styles – outline, solid, and mini.
-  By default, the outline style is used, but solid and mini may
-  be applied by using the `-solid` and `-mini` suffix.
-
-  You can customize the size and colors of the icons by setting
-  width, height, and background color classes.
-
-  Icons are extracted from your `assets/vendor/heroicons` directory and bundled
-  within your compiled app.css by the plugin in your `assets/tailwind.config.js`.
-
-  ## Examples
-
-      <.icon name="hero-x-mark-solid" />
-      <.icon name="hero-arrow-path" class="ml-1 w-3 h-3 animate-spin" />
-  """
-  attr :name, :string, required: true
-  attr :class, :string, default: nil
-
-  def icon(%{name: "hero-" <> _} = assigns) do
-    ~H"""
-    <span class={[@name, @class]} />
     """
   end
 
