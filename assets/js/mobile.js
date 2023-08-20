ba95b2946c79d2d901858ccffded4038929b5550
phaleth <29838606+phaleth@users.noreply.github.com>
2023-08-20 16:13:56 +0200

    Make the frontpage responsive and add icons to hide sidebars

---
+++ assets/js/mobile.js
@@ -0,0 +1,35 @@
+export const refreshSidebars = () => {
+  const chansListEl = document.querySelector(".wxch-chans-list");
+  const chansIcoBurgerEl = document.querySelector(".wxch-chans-ico-hamburger");
+  const chansIcoCloseEl = document.querySelector(".wxch-chans-ico-close");
+  document.querySelector(".wxch-chans-btn").onclick = () => {
+    chansListEl.classList.toggle("hidden");
+    chansIcoBurgerEl.classList.toggle("hidden");
+    chansIcoCloseEl.classList.toggle("hidden");
+  };
+
+  const usersListEl = document.querySelector(".wxch-users-list");
+  const usersIcoBurgerEl = document.querySelector(".wxch-users-ico-hamburger");
+  const usersIcoCloseEl = document.querySelector(".wxch-users-ico-close");
+  document.querySelector(".wxch-users-btn").onclick = () => {
+    usersListEl.classList.toggle("hidden");
+    usersIcoBurgerEl.classList.toggle("hidden");
+    usersIcoCloseEl.classList.toggle("hidden");
+  };
+
+  let resizeTimeout;
+  window.onresize = () => {
+    let width = window.innerWidth;
+    clearTimeout(resizeTimeout);
+    resizeTimeout = setTimeout(() => {
+      if (width > 639) {
+        chansListEl.classList.toggle("hidden");
+        chansIcoBurgerEl.classList.toggle("hidden");
+        chansIcoCloseEl.classList.toggle("hidden");
+        usersListEl.classList.toggle("hidden");
+        usersIcoBurgerEl.classList.toggle("hidden");
+        usersIcoCloseEl.classList.toggle("hidden");
+      }
+    }, 250);
+  };
+};
