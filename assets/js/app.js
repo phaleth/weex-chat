e9a296949a4b051614e025632b587690e47810fa
phaleth <29838606+phaleth@users.noreply.github.com>
2023-08-26 10:27:46 +0200

    Adjust topbar

--- assets/js/app.js
+++ assets/js/app.js
@@ -33,8 +33,11 @@ const liveSocket = new LiveSocket("/live", Socket, {
 });
 
 // Show progress bar on live navigation and form submits
-topbar.config({ barColors: { 0: "#29d" }, shadowColor: "rgba(0, 0, 0, .3)" });
-window.addEventListener("phx:page-loading-start", (_info) => topbar.show(300));
+topbar.config({
+  barColors: { 0: "#19554f" },
+  shadowColor: "rgba(0, 0, 0)",
+});
+window.addEventListener("phx:page-loading-start", (_info) => topbar.show(400));
 window.addEventListener("phx:page-loading-stop", (_info) => topbar.hide());
 
 refreshSidebars();
