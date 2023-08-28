9c772c92e1de2e2282445667a12f6d5e316cb0f7
phaleth <29838606+phaleth@users.noreply.github.com>
2023-08-28 22:18:50 +0200

    Add sign in/out user menu that shows on hover

--- test/weex_chat_web/live/user_registration_live_test.exs
+++ test/weex_chat_web/live/user_registration_live_test.exs
@@ -40,8 +40,8 @@ defmodule WeexChatWeb.UserRegistrationLiveTest do
     test "creates account and logs the user in", %{conn: conn} do
       {:ok, lv, _html} = live(conn, ~p"/users/register")
 
-      email = unique_user_email()
-      form = form(lv, "#registration_form", user: valid_user_attributes(email: email))
+      username = unique_user_name()
+      form = form(lv, "#registration_form", user: valid_user_attributes(username: username))
       render_submit(form)
       conn = follow_trigger_action(form, conn)
 
@@ -50,7 +50,7 @@ defmodule WeexChatWeb.UserRegistrationLiveTest do
       # Now do a logged in request and assert on the menu
       conn = get(conn, "/")
       response = html_response(conn, 200)
-      assert response =~ email
+      assert response =~ username
       assert response =~ "Settings"
       assert response =~ "Log out"
     end
